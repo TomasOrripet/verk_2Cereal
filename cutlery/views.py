622fefd2dab4afda27e85e2c189128aa8293de95
TomasOrripet <TomasOrripet>
2021-05-03 13:22:17 +0000

    htmls

--- cutlery/views.py
+++ cutlery/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse('this displays cutlery')
+    return render(request, 'cutlery/index.html')
--- sign_up/views.py
+++ sign_up/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('this is our sign up')
+    return render(request, 'sign_up/index.html')
