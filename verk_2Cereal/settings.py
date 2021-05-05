af91804ecd2adfbcd67b69b03dba264937563fee
TomasOrripet <TomasOrripet>
2021-05-05 14:04:12 +0000

    Merge branch 'master' into tomas3

--- verk_2Cereal/settings.py
+++ verk_2Cereal/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'verk_2Cereal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'ship-O-cereal-db',
         'USER': 'tomasorri',
         'PASSWORD': 'tomas123',
