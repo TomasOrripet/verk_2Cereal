6820eac47d81e5a7d127c0177862de35bfa2ea9a
onni2 <70693642+onni2@users.noreply.github.com>
2021-05-05 10:25:23 +0000

    createCereal

--- cereal/urls.py
+++ cereal/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     # http://localhost:8000/sign in
     path('', views.index, name='cereal-index'),
+    path('createCereal', views.createCereal, name="createCereal")
 ]
--- cereal/views.py
+++ cereal/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from cereal.forms.cereal_form import cerealCreateForm
 
 
-# Create your views here.
 def index(request):
     return render(request, 'cereal/index.html')
+
+
+def createCereal(request):
+    if request.method == 'POST':
+        form = cerealCreateForm(data=request.POST)
+        if form.is_valid():
+            cereal = form.save()
+            return redirect('cereal-index')
+    else:
+        form = cerealCreateForm()
+    return render(request, 'cereal/createCereal.html', {
+        'form': form
+    })
