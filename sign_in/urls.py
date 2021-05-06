55eed6dd878b3e31594204292117f57d83108e02
aldis19-fjall <70693283+aldis19-fjall@users.noreply.github.com>
2021-05-06 11:13:54 +0000

    uppdate sign in

--- sign_in/urls.py
+++ sign_in/urls.py
@@ -1,7 +1,11 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from . import views
 
 urlpatterns = [
     # http://localhost:8000/sign in
     path('', views.index, name='sign_in-index'),
+    path('login', LoginView.as_view(template_name='sign_in/index.html'), name='sign_in-index'),
+    path('logout', LogoutView.as_view(next_page='login'), name='logout')
+
 ]
