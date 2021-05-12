a1a65763b57c82a8926e788cc9f7d7dfc4ac011c
aldis19-fjall <70693283+aldis19-fjall@users.noreply.github.com>
2021-05-12 20:38:07 +0000

    uppdata filterby

--- toys/urls.py
+++ toys/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     # http://localhost:8000/toys
     path('', views.index, name='toys-index'),
-    path('<int:id>', views.toyInfo, name='toy_info')
+    path('<int:id>', views.toyInfo, name='toy_info'),
+    path('priceResultToys', views.orderByPrice, name='priceResultToys'),
+    path('filterResultToys', views.filterBy, name='filterResultToys'),
+    path('searchResultToys', views.searchResultToysView, name='searchResultToys'),
     #path('createtoy', views.toys, name='create-toy')
 ]
