b96b358250edd371ec0a5bf226f8d74edcb64910
onni2 <70693642+onni2@users.noreply.github.com>
2021-05-12 14:29:04 +0000

    footer

--- account/views.py
+++ account/views.py
@@ -1,23 +1,23 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from account.form.form import imageForm
+from account.form.form import profileForm
 from account.models import Profile
 from django.core.files.storage import FileSystemStorage
 from django.contrib.auth.models import User
 
 
 @login_required
-def ImageForm(request, id):
+def profile(request):
+    profile = User.objects.filter(user=request.user).first()
     if request.method == 'POST':
-        form = imageForm(data=request.POST)
+        form = profileForm(instance=profile, data=request.POST)
         if form.is_valid():
-            form.save()
-            content = {"user": get_object_or_404(User, id=id)}
-            return render(request, 'account/userInfo.html', content)
-    else:
-        form = imageForm()
-    return render(request, 'userInfo.html', {
-        'form': form
+            profile = form.save(commit=False)
+            profile.user = request.user
+            profile.save()
+            return redirect('userInfo')
+    return render(request, 'account/userInfo.html',{
+        'form':profileForm(instance=profile)
     })
 
 # Create your views here.
