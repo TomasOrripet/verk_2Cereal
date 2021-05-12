6bffb871d21fc0c75fc4a943d24e534a5ee5a53c
onni2 <70693642+onni2@users.noreply.github.com>
2021-05-12 10:47:07 +0000

    filter

--- account/views.py
+++ account/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
-
+from account.form.form import ImageForm
 from django.contrib.auth.models import User
 
-
+def image_upload_view(request):
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            profilePic = form.instance
+            return render(request, 'userInfo', {'form': form, 'profilePic': profilePic})
+    else:
+        form = ImageForm()
+    return render(request, 'index.html', {'form': form})
 
 # Create your views here.
 @login_required
