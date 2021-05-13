28d5e9fe0db39268c5dfab2dfe5e89658faba8d0
TomasOrripet <TomasOrripet>
2021-05-13 13:44:43 +0000

    bættum in amount í cart

--- cart/urls.py
+++ cart/urls.py
@@ -5,5 +5,4 @@ from . import views
 urlpatterns = [
     #http://localhost:8000/Homepage/cart
     path('', views.index, name='cart-index'),
-    path('<int:id>', views.addToCart, name='cart-add')
 ]
