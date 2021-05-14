5acf8676c40bfcb7fbe37cc134f109df92d1decc
TomasOrripet <TomasOrripet>
2021-05-14 11:59:31 +0000

    select country

--- payment/form/form.py
+++ payment/form/form.py
@@ -15,11 +15,14 @@ class cardForm(ModelForm):
         }
 
 class userInfoForm(ModelForm):
+
+
     class Meta:
+        country_choices = [(1, 'Iceland'), (2, 'Norway'), (3, 'Denmark'), (4, 'Istanbul'), (5,'Kazakhstan')]
         model = userInfo
         exclude = ['id', 'user']
         widgets = {
-            'country': widgets.TextInput(attrs={'class': 'form-control'}),
+            'country': widgets.Select(choices=country_choices),
             'city': widgets.TextInput(attrs={'class': 'form-control'}),
             'address': widgets.TextInput(attrs={'class': 'form-control'}),
             'zip': widgets.NumberInput(attrs={'class': 'form-control'}),
